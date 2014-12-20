f3be0c22a8e7713ae5e961025d4472eb5e93ee61
Tomotaka Sakuma <ktmtmks@gmail.com>
2014-12-20 18:21:37 +0900

    a test with jasmine which will fail

---
+++ spec/HelloSpec.js
@@ -0,0 +1,8 @@
+// http://pivotal.github.io/jasmine/
+describe("Hello Test", function() {
+  it("test", function() {
+    var a = 'test1';
+    var e = 'test';
+    expect(a).toEqual(e);
+  });
+});
